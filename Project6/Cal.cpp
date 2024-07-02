b73006cca617c77669caf853c8a59f60fe4dced8
seulgiishin <tls940807@gmail.com>
2024-07-02 16:13:45 +0900

    getSum & unit test

--- Project6/Cal.cpp
+++ Project6/Cal.cpp
@@ -1,6 +1,8 @@
 class Cal
 {
 public:
-
+	int getSum(int a, int b) {
+		return a + b;
+	}
 
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -5,3 +5,9 @@ TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
 }
+
+TEST(TestCaseName, TestGetSum) {
+	Cal* c = new Cal();
+
+	EXPECT_EQ(c->getSum(1, 2), 3);
+}
