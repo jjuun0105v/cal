68b02a7884e8eafc2ee9adfe1270a836af19cf9f
changhyun-cra <hellow6044@gmail.com>
2024-07-02 16:25:58 +0900

    add getSumSum

--- Project6/Cal.cpp
+++ Project6/Cal.cpp
@@ -1,6 +1,8 @@
 class Cal
 {
 public:
-
+	int getSumSum(int a, int b, int c) {
+		return a + b + c;
+	}
 
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,7 +1,13 @@
 #include "pch.h"
 #include "../Project6/Cal.cpp"
 
-TEST(TestCaseName, TestName) {
+TEST(TestCaseName, Testz) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
+}
+
+TEST(TestCaseName, TestSumSum) {
+	Cal cal = Cal();
+
+	EXPECT_EQ(cal.getSumSum(1, 2, 3), 6);
 }
