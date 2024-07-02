545df5aae9619cdba66a831cee0fae7024642611
juheon.song <bootplus@naver.com>
2024-07-02 16:41:26 +0900

    unit test 추가

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -5,3 +5,9 @@ TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
 }
+
+TEST(TestCaseName, getDivide) {
+	Cal cal;
+	EXPECT_EQ(cal.getDivide(2, 2), 1);
+	EXPECT_EQ(cal.getDivide(1, 2), 0.5);
+}
